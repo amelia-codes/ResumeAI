6803e3c24db863f7582558c9abe6cff5570f6844
j72jones <jjones690@gatech.edu>
2025-08-06 17:38:21 -0400

    Moved db and added gitignore

--- sentence_comparison.py
+++ sentence_comparison.py
@@ -1,4 +1,21 @@
-from sentence_transformers import SentenceTransformer, util
+#from sentence_transformers import SentenceTransformer, util
+import sqlite3
+
+job_name = "math"
+
+connection = sqlite3.connect("Database/ONET_DATABASE.db")
+cursor = connection.cursor()
+
+cursor.execute(f"SELECT onetsoc_code, title FROM occupation_data WHERE title LIKE '%{job_name}%';")
+print(cursor.fetchall())
+
+cursor.execute(f"SELECT onetsoc_code, alternate_title, short_title FROM alternate_titles WHERE alternate_title LIKE '%{job_name}%' OR short_title LIKE '%{job_name}%';")
+print(cursor.fetchall())
+
+cursor.execute(f"SELECT onetsoc_code, reported_job_title FROM sample_of_reported_titles WHERE reported_job_title LIKE '%{job_name}%';")
+print(cursor.fetchall())
+
+quit()
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
