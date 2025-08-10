6a198a670ad28dcda6ca56f7edb127b65d7c64b0
j72jones <jjones690@gatech.edu>
2025-08-10 15:10:23 -0400

    Target dataset creator, but not created...

---
+++ Database/target_dataset_creator.py
@@ -0,0 +1,25 @@
+import sqlite3
+import pandas as pd
+import spacy
+
+connection = sqlite3.connect("Database/ONET_DATABASE.db")
+cursor = connection.cursor()
+query = """
+    SELECT 
+        description
+    FROM content_model_reference
+"""
+cursor.execute(query)
+occupation_list = cursor.fetchall()
+
+print("check 1")
+
+full_list = []
+nlp = spacy.load("en_core_web_trf")
+for section_text in occupation_list:
+    tokens = nlp(section_text[0])
+    full_list.extend(list(tokens.noun_chunks))
+
+print("check 2")
+
+pd.DataFrame(full_list).to_excel("Database/target_dataset.xlsx")
