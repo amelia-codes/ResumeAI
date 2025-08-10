673e405614d9b611b33f0b5248b66a279e280ea6
j72jones <jjones690@gatech.edu>
2025-08-10 18:05:28 -0400

    Finished target_dataset

--- Database/target_dataset_creator.py
+++ Database/target_dataset_creator.py
@@ -4,21 +4,32 @@ import spacy
 
 connection = sqlite3.connect("ONET_DATABASE.db")
 cursor = connection.cursor()
-query = """
-    SELECT 
-        description
-    FROM content_model_reference
-"""
-cursor.execute(query)
-occupation_list = cursor.fetchall()
+occupation_list = []
+for table in ('skills', 'knowledge', 'abilities', 'work_activities'):
+    query = f"""
+    SELECT
+        cmr.description
+    FROM occupation_data oc
+    RIGHT JOIN {table} sk
+        ON oc.onetsoc_code = sk.onetsoc_code
+    LEFT JOIN scales_reference sr
+        ON sk.scale_id = sr.scale_id
+    LEFT JOIN content_model_reference cmr
+        ON sk.element_id = cmr.element_id
+    WHERE sk.scale_id = 'IM'
+    AND sk.data_value > 2
+    """
+    cursor.execute(query)
+    occupation_list.extend(cursor.fetchall())
 
 print("check 1")
 
 full_list = []
 nlp = spacy.load("en_core_web_trf")
-for section_text in occupation_list:
-    tokens = nlp(section_text[0])
-    full_list.extend([" ".join(noun_chunk) for noun_chunk in tokens.noun_chunks])
+for i in range(0, len(occupation_list), 5):
+    tokens = nlp("\n".join(section_text[0] for section_text in occupation_list[i: min([i+5, len(occupation_list)])]))
+    full_list.extend([" ".join(token.text for token in noun_chunk) for noun_chunk in tokens.noun_chunks])
+  
 
 print("check 2")
 
