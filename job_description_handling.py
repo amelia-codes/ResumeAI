c8f406389704373d87d015650f881a48a7d1eb21
j72jones <jjones690@gatech.edu>
2025-08-17 17:29:01 -0400

    Initial structure file functions

--- job_description_handling.py
+++ job_description_handling.py
@@ -1,2 +1,7 @@
+import spacy
+
 def job_description_phrase_scraper(job_description: str) -> list[str]:
-    pass
+    nlp = spacy.load("en_core_web_trf")
+    tokens = nlp(job_description)
+    phrases = list(tokens.noun_chunks)
+    return phrases
--- resume_handling.py
+++ resume_handling.py
@@ -1,4 +1,21 @@
+import spacy
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
 def resume_phrase_scraper(resume_experience_description: str) -> list[str]:
-    pass
-def resume_ranker(resume_experience_phrase_list: list[str]) -> float:
-    pass
+    nlp = spacy.load("en_core_web_trf")
+    tokens = nlp(resume_experience_description)
+    phrases = list(tokens.noun_chunks)
+    return phrases
+
+
+def resume_ranker(resume_experience_phrase_list: list[str], job_description_phrase_list: list[str]) -> float:
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    job_description_embeddings = [model.encode(phrases) for phrases in job_description_phrase_list]
+
+    resume_experience_embeddings = [model.encode(phrases) for phrases in resume_experience_phrase_list]
+    resume_scores = []
+    for resume_embedding in resume_experience_embeddings:
+        resume_scores.append(np.mean([model.similarity(resume_embedding, job_description_embedding) for job_description_embedding in job_description_embeddings]))
+
+    return resume_scores
