425f836db178e6f8c7ab300fe4284b8e41fba7e1
j72jones <jjones690@gatech.edu>
2025-08-17 17:17:26 -0400

    Adding structure files

---
+++ job_description_handling.py
@@ -0,0 +1,2 @@
+def job_description_phrase_scraper(job_description: str) -> list[str]:
+    pass
---
+++ resume_handling.py
@@ -0,0 +1,4 @@
+def resume_phrase_scraper(resume_experience_description: str) -> list[str]:
+    pass
+def resume_ranker(resume_experience_phrase_list: list[str]) -> float:
+    pass
